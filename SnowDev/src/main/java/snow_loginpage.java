e3bc50e29a49e7df90191b3ccee9e5bd87446f56
brgujjula3gs <76172053+brgujjula3gs@users.noreply.github.com>
2020-12-25 00:58:52 +0530

    My first code

--- SnowDev/src/main/java/snow_loginpage.java
+++ SnowDev/src/main/java/snow_loginpage.java
@@ -1,2 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class snow_loginpage {
+
+    public static void main (String args[]) throws InterruptedException {
+
+       System.setProperty("webdriver.chrome.driver","D:\\Java\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://automationpractice.com/index.php");
+        Thread.sleep(5000);
+
+        driver.findElement(By.linkText("Sign in")).click();
+        System.out.println("clicked on sign in");
+
+    }
 }
